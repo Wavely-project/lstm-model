d3402acd79ea60071e150fd6715817da0c1d4225
Sara Hamza <sarasaadoun4@gmail.com>
2024-05-08 04:50:54 +0200

    add new lstm model to app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import mediapipe as mp
 app = Flask(__name__)
 
 # Load the model
-model = load_model('action.h5')
-actions = np.array(['hello', 'thanks', 'iloveyou'])
+model = load_model('./model_lstm_6_classes_0.98.h5')
+actions = np.array(['drink', 'eat', 'goodbye', 'hello', 'help', 'how are you'])
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilitiesactions = np.array(['hello', 'thanks', 'iloveyou'])
@@ -35,31 +35,73 @@ def extract_keypoints(results):
     return np.concatenate([pose, face, lh, rh])
 
 def get_prediction(video_path):
-    # Read the video file
-    cap = cv2.VideoCapture(video_path)
+
+    # Load the model and define other necessary variables
     sequence = []
-    print("start.")
+    sentence = []
+    predictions = []
+    threshold = 0.5
+
+    # Load the video file
+    cap = cv2.VideoCapture(video_path)
+    start_time = time.time()
+    print('start')
+    print("total frames count: ",int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
     while cap.isOpened():
+        # Read a frame from the video
         ret, frame = cap.read()
 
+        # If there are no more frames, break the loop
         if not ret:
+            print("No more frames. Exiting loop.")
             break
 
+        # print("Frame read successfully.")
+
+        # Make detections
         with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             image, results = mediapipe_detection(frame, holistic)
+            # print("Detections made.")
 
+        # Prediction logic
         keypoints = extract_keypoints(results)
-        sequence.append(keypoints)
+        lh_rh = keypoints[1536:]
+        #Remove z Axis From Landmarks
+        for z in range(2,lh_rh.shape[0],3):
+            lh_rh[z] = None
+        #Remove NaN Data
+        lh_rh = lh_rh[np.logical_not(np.isnan(lh_rh))]
+        sequence.append(lh_rh)
         sequence = sequence[-30:]
-
+        # print("keypoints extracted.")
         if len(sequence) == 30:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(f"Model prediction made. {actions[np.argmax(res)]}")
+            predictions.append(np.argmax(res))
 
-    # Make predictions and return the result
-    prediction_index = np.argmax(res)
-    prediction = actions[prediction_index]
-    print("end")
-    return jsonify({'prediction': prediction})
+            if np.unique(predictions[-10:])[0] == np.argmax(res):
+                if res[np.argmax(res)] > threshold:
+                    if len(sentence) > 0:
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+            # sequence = []
+            # if len(sentence) > 5:
+            #     sentence = sentence[-5:]
+
+        
+
+    # Calculate elapsed time
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("end.\nTotal time taken: {:.2f} seconds".format(elapsed_time))
+    # Release the video capture object and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+   
+    return jsonify({'prediction': sentence})
 
 app = Flask(__name__) 
 
